c30202c89386c81f418d2d99f5b79de2131d4c12
Happy-Love <yutabox@mail.ru>
2020-03-01 15:22:18 +0300

    rake DCM  Confirm Change [lab2]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :firstname, :lastname)
+    params.require(:user).permit(:email, :firstname, :lastname, :password)
   end
 end
