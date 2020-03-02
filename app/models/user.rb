3643644c94f088ffa1a7b7ef52e187fc6dd868cf
Happy-Love <yutabox@mail.ru>
2020-03-02 13:32:38 +0300

    confirmation email [lab2]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates_presence_of :email
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users 
   get 'users/show', as: 'user_root'
   root 'home#index'
 end
