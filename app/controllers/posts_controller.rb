e0a0ffdd0757560e939e65f91265147563ffc101
Happy-Love <yutabox@mail.ru>
2020-04-30 02:29:03 +0300

    users/1 [posts]

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,10 @@ class PostsController < ApplicationController
     Post.create(post_params)
     redirect_to root_path
   end
+  
+  def index
+    @posts = Post.all
+  end
 
   def show
     @post = Post.find(params[:id])
