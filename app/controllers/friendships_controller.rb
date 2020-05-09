88143181193e4db2bc89317ed41d30d170a1e90a
Happy-Love <yutabox@mail.ru>
2020-05-09 06:23:23 +0300

    Followers [lab4] ready to merge in master!

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,12 +3,12 @@ class FriendshipsController < ApplicationController
   before_action :find_user
   def create
     current_user.follow(@user)
-    redirect_to users_path    
+    redirect_to user_path(@user.id)    
   end
 
   def destroy
     current_user.unfollow(@user)
-    redirect_to users_path    
+    redirect_to user_path(@user.id)    
   end
   
   private
