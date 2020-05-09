3d729fe81fa2a419b2317393e8ce76b0a1a584a8
Happy-Love <yutabox@mail.ru>
2020-05-09 06:15:18 +0300

    Followers with controllers, views, counters [lab4]

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users  
   resources :users do
+    member do
+      get :following, :followers
+    end  
     resources :posts
   end
+  resources :friendships, only: %i[create destroy]
   get 'users' => 'users#index', as: 'user_root'
   root 'home#index'
 end
